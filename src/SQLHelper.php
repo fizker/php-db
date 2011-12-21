c51271add206621483b09fe85d238cf66b5a4f30
Benjamin Horsleben <benjamin@fizkerinc.dk>
2011-12-21 09:25:31 +0100

    Select/Insert statements - Acceptable insert statements are possible - Select is at an acceptable level.

--- src/SQLHelper.php
+++ src/SQLHelper.php
@@ -23,12 +23,21 @@ class SQLHelper {
 		return $s->select($what);
 	}
 	
+	public function insert($data) {
+		$i = new InsertBuilder();
+		return $i->insert($data);
+	}
+	
+	public function update() {
+		return new UpdateBuilder();
+	}
+	
 	public function setDebug() {}
 }
 
 class SelectBuilder {
 	private $db, $prefix;
-	private $what, $from, $where;
+	private $what, $from, $where, $order;
 	
 	public function __construct($db, $prefix = '') {
 		$this->db = $db;
@@ -77,12 +86,51 @@ class SelectBuilder {
 		$this->where = $where;
 		return $this;
 	}
+	public function order($order) {
+		$this->order = $order;
+		return $this;
+	}
 	public function exec() {
 		$query = "SELECT $this->what FROM $this->from";
 		if($this->where)
 			$query .= " WHERE $this->where";
+		if($this->order)
+			$query .= ' ORDER BY '.$this->order;
 		
 		return $query;
+	}
+}
+
+class InsertBuilder {
+	private $data, $table;
+	
+	public function insert($data) {
+		$this->data = $data;
+		return $this;
+	}
+	public function into($table) {
+		$this->table = $table;
+		return $this;
+	}
+	public function exec() {
+		$cols = array();
+		$vals = array();
+		foreach($this->data as $col=>$val) {
+			$cols[] = '`'.$col.'`';
+			$vals[] = '"'.str_replace('"', '\\"', $val).'"';
+		}
+		$query = 'INSERT INTO db.'.$this->table
+			.' ('.implode(', ', $cols).') VALUES ('.implode(', ', $vals).')';
+		return $query;
+	}
+}
+
+class UpdateBuilder {
+	public function set() {
+		return $this;
+	}
+	public function exec() {
+		return 'UPDATE db.table SET `a`="b"';
 	}
 }
 ?>
--- unittests/SQLHelperTest.php
+++ unittests/SQLHelperTest.php
@@ -102,7 +102,6 @@ class SQLHelperTest extends PHPUnit_Framework_TestCase {
 		
 		$this->assertEquals("SELECT a FROM $database.b", $result);
 	}
-
 	public function provider_select_DatabaseIsSet_TablesArePrefixed() {
 		return array(
 			array('db'), 
@@ -111,6 +110,72 @@ class SQLHelperTest extends PHPUnit_Framework_TestCase {
 		);
 	}
 
+	/**
+	 * @test
+	 */
+	public function select_OrderIsGiven_TheDataIsOrdered() {
+		$db = $this->createHelper();
+		
+		$result = $db->select('a')->from('b')->order('a DESC')->exec();
+		
+		$this->assertEquals('SELECT a FROM db.b ORDER BY a DESC', $result);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider provider_insert_SingleValue_InsertsSingleValue
+	 */
+	public function insert_SingleValue_InsertsSingleValue($col, $value) {
+		$db = $this->createHelper();
+		
+		$result = $db->insert(array($col=>$value))->into('c')->exec();
+		
+		$this->assertEquals("INSERT INTO db.c (`$col`) VALUES (\"$value\")", $result);
+	}
+	public function provider_insert_SingleValue_InsertsSingleValue() {
+		return array(
+			array('a', 'b'),
+			array('c', 'd')
+		);
+	}
+	
+	/**
+	 * @test
+	 */
+	public function insert_MultipleValues_InsertsAll() {
+		$db = $this->createHelper();
+		
+		$result = $db->insert(array(
+			'a'=>'b',
+			'c'=>'d'
+		))->into('table')->exec();
+		
+		$this->assertEquals('INSERT INTO db.table (`a`, `c`) VALUES ("b", "d")', $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function insert_ValueWithQuotes_ItShouldEscapeTheQuotes() {
+		$db = $this->createHelper();
+		
+		$result = $db->insert(array('a'=>'b"c'))->into('table')->exec();
+		
+		$this->assertEquals('INSERT INTO db.table (`a`) VALUES ("b\"c")', $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function update_SingleValueNoWhere_ValueIsSet() {
+		$db = $this->createHelper();
+		
+		$result = $db->update('table')->set(array(
+			'a'=>'b'
+		))->exec();
+		
+		$this->assertEquals('UPDATE db.table SET `a`="b"', $result);
+	}
 
 	public function createHelper() {
 		$db = new SQLHelper(array(
