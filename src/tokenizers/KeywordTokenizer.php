7d4972a0f875c07be485d385930bf225f0f862d7
Benjamin Horsleben <benjamin@fizkerinc.dk>
2013-03-21 14:08:28 +0100

    KeywordTokenizing seems complete. - It will indirectly be tested further by being used going forward.

--- src/tokenizers/KeywordTokenizer.php
+++ src/tokenizers/KeywordTokenizer.php
@@ -10,24 +10,29 @@ class KeywordTokenizer {
 	private $input, $keywords;
 	public function __construct($str, $keywords) {
 		$this->input = $str;
-		$this->keywords = $keywords;
+		$this->keywords = $keywords = array_map(function($w) {
+			return new Keyword($w);
+		}, $keywords);
 
 		$this->results = array();
 		$words = explode(' ', $str);
 		$nextWord = '';
 		foreach($words as $word) {
-			if(in_array($word, $keywords)) {
-				if($nextWord) {
-					$this->results[] = new Keyword(trim($nextWord), false);
-					$nextWord = '';
-				}
-				$this->results[] = new Keyword($word, true);
-				continue;
-			}
 			$nextWord .= ' ' . $word;
+
+			foreach($keywords as $keyword) {
+				if($keyword->matches($word)) {
+					$nextWord = substr($nextWord, 0, -strlen($keyword->keyword));
+					if($nextWord) {
+						$this->results[] = new Token(trim($nextWord), false);
+						$nextWord = '';
+					}
+					$this->results[] = new Token($keyword->keyword, true);
+				}
+			}
 		}
 		if($nextWord) {
-			$this->results[] = new Keyword(trim($nextWord), false);
+			$this->results[] = new Token(trim($nextWord), false);
 		}
 	}
 
@@ -42,6 +47,42 @@ class KeywordTokenizer {
 }
 
 class Keyword {
+	private $words, $partialWords = array();
+	public $keyword;
+	public function __construct($keyword) {
+		$this->words = sizeof(explode(' ', $keyword));
+		$this->keyword = $keyword;
+	}
+
+	public function matches($word) {
+		// Incrementing the list of partial words
+		$p = array();
+		foreach($this->partialWords as $partial) {
+			$p[] = array(
+				$partial[0] + 1,
+				$partial[1] . ' ' . $word
+			);
+		}
+		$p[] = array(1, $word);
+
+		// The word to match against
+		$word = $p[0];
+
+		// Removing the top if it has exceeded the keyword length
+		if($word[0] > $this->words) {
+			array_shift($p);
+			$word = $p[0];
+		}
+		$this->partialWords = $p;
+
+		// Getting the actual string
+		$word = $word[1];
+
+		return $word === $this->keyword;
+	}
+}
+
+class Token {
 	public $value, $isKeyword;
 	public function __construct($value, $isKeyword) {
 		$this->value = $value;
--- tests/unittests/tokenizers/KeywordTokenizerTest.php
+++ tests/unittests/tokenizers/KeywordTokenizerTest.php
@@ -50,4 +50,82 @@ class KeywordTokenizerTest extends PHPUnit_Framework_TestCase {
 
 		$this->assertNull($p4);
 	}
+
+
+	/**
+	 * @test
+	 */
+	public function next_quotedKeyword_iteratesProperly() {
+		$keywords = new KeywordTokenizer('ab `EF` EF gh', array('EF'));
+
+		$p1 = $keywords->next();
+		$p2 = $keywords->next();
+		$p3 = $keywords->next();
+		$p4 = $keywords->next();
+
+		$this->assertEquals('ab `EF`', $p1->value);
+		$this->assertFalse($p1->isKeyword);
+
+		$this->assertEquals('EF', $p2->value);
+		$this->assertTrue($p2->isKeyword);
+
+		$this->assertEquals('gh', $p3->value);
+		$this->assertFalse($p3->isKeyword);
+
+		$this->assertNull($p4);
+	}
+
+	/**
+	 * @test
+	 */
+	public function next_keywordHasSpaces_iteratesProperly() {
+		$keywords = new KeywordTokenizer('ab CD EF gh', array('CD EF'));
+
+		$p1 = $keywords->next();
+		$p2 = $keywords->next();
+		$p3 = $keywords->next();
+		$p4 = $keywords->next();
+
+		$this->assertEquals('ab', $p1->value);
+		$this->assertFalse($p1->isKeyword);
+
+		$this->assertEquals('CD EF', $p2->value);
+		$this->assertTrue($p2->isKeyword);
+
+		$this->assertEquals('gh', $p3->value);
+		$this->assertFalse($p3->isKeyword);
+
+		$this->assertNull($p4);
+	}
+
+	/**
+	 * @test
+	 */
+	public function next_multipleKeywords_iteratesProperly() {
+		$keywords = new KeywordTokenizer('ab CD ef GH ij', array('CD', 'GH'));
+
+		$p1 = $keywords->next();
+		$p2 = $keywords->next();
+		$p3 = $keywords->next();
+		$p4 = $keywords->next();
+		$p5 = $keywords->next();
+		$p6 = $keywords->next();
+
+		$this->assertEquals('ab', $p1->value);
+		$this->assertFalse($p1->isKeyword);
+
+		$this->assertEquals('CD', $p2->value);
+		$this->assertTrue($p2->isKeyword);
+
+		$this->assertEquals('ef', $p3->value);
+		$this->assertFalse($p3->isKeyword);
+
+		$this->assertEquals('GH', $p4->value);
+		$this->assertTrue($p4->isKeyword);
+
+		$this->assertEquals('ij', $p5->value);
+		$this->assertFalse($p5->isKeyword);
+
+		$this->assertNull($p6);
+	}
 }
